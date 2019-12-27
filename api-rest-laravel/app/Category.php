dfeb8d88527c54e96570c72344280c643d067d92
PaxAuz15 <luisauz.geek@gmail.com>
2019-12-26 19:11:50 -0500

    feat: relationship for show data on the models.

--- api-rest-laravel/app/Category.php
+++ api-rest-laravel/app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // utilizar tabla de la base de datos
+    protected $table = 'categories';
+    
+    //Relacion de uno a muchos
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
--- api-rest-laravel/app/Post.php
+++ api-rest-laravel/app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+/*Relacion de muchos a uno 
+    -Muchos posts creados por un usuario
+    -Muchos posts tienen una categoria */
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Category','category_id');
+    }
 }
