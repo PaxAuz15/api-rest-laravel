a41b39de5cbbe5e5a1e3d21e4df62c9e15c2ca56
PaxAuz15 <luisauz.geek@gmail.com>
2019-12-26 20:18:24 -0500

    feat: Routes with gest & post HTTP protocols
    The post protocols doesn't checking yet

--- api-rest-laravel/app/Http/Controllers/UserController.php
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function test(Request $request){
+        return "Test action from UserController";
+    }
+
+    public function register(Request $request){
+        return "Register action to user";
+    }
+
+    public function login(Request $request){
+        return "Login action to user";
+    }
 }
--- api-rest-laravel/routes/web.php
+++ api-rest-laravel/routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//API  ROUTES
+
+    //TESTING ROUTES
+    Route::get('/user/test', 'UserController@test');
+    Route::get('/category/test', 'CategoryController@test');
+    Route::get('/post/test', 'PostController@test');
+
+    //USER CONTROLLER ROUTES
+    Route::post('/api/register', 'UserController@register');
+    Route::post('/api/login','UserController@login');
